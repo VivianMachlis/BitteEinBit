8bb58fea08e0ef8207266fc8de172f5c59bf839b
VIVI\Vivian <vivian.machlis@web.de>
2017-02-13 16:29:41 +0100

    Changed: app-routing.module.ts - Seda, Fritz, Vivi

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@
     import { ContentComponent }  from './content/content.component';
     import {LoginComponent}  from './login/login.component';
     import {ChangepasswordComponent} from './changepassword/changepassword.component';
+    import {DeleteprofileComponent} from './deleteprofile/deleteprofile.component';
 
     const routes: Routes = [
       //initial path to login
@@ -16,6 +17,11 @@
           {path:'', component:FooterComponent,outlet:'footer'},
           {path:'', component:HeaderComponent,outlet:'header'},
           {path:'', component:ChangepasswordComponent,outlet:'content'},
+      ]},
+      { path: 'deleteprofile', children: [
+          {path:'', component:FooterComponent,outlet:'footer'},
+          {path:'', component:HeaderComponent,outlet:'header'},
+          {path:'', component:DeleteprofileComponent,outlet:'content'},
       ]},
       //standard when logged in
       { path: 'achievedCompetences',  children: [
